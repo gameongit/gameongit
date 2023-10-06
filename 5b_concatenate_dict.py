e282e57759d8169446b1b291e0957cd36a6b94ac
gameongit <146956767+gameongit@users.noreply.github.com>
2023-10-07 00:33:15 +0530

    Delete 5b_concatenate_dict.py

--- 5b_concatenate_dict.py
+++
@@ -1,9 +0,0 @@
-Python 3.11.5 (tags/v3.11.5:cce6ba9, Aug 24 2023, 14:38:34) [MSC v.1936 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> dic1={1:10,2:20}
->>> dic2={4:40,5:50}
->>> print(dic1)
-{1: 10, 2: 20}
->>> dic1.update(dic2)
->>> print(dic1)
-{1: 10, 2: 20, 4: 40, 5: 50}
