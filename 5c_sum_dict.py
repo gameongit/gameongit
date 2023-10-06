e08f4b76418e628f281fb36395c4e27564472929
gameongit <146956767+gameongit@users.noreply.github.com>
2023-10-07 00:34:24 +0530

    Delete 5c_sum_dict.py

--- 5c_sum_dict.py
+++
@@ -1,5 +0,0 @@
-Python 3.11.5 (tags/v3.11.5:cce6ba9, Aug 24 2023, 14:38:34) [MSC v.1936 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> rate={'milk':30,'frankie':50,'travelling':12}
->>> sum(rate.values())
-92
