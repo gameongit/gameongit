043a8da9c535c951422bdcd5114c3c1cb639214d
gameongit <146956767+gameongit@users.noreply.github.com>
2023-10-07 00:30:25 +0530

    Delete 1d_reverse-value.py

--- 1d_reverse-value.py
+++
@@ -1,8 +0,0 @@
-# user_value = input("Enter a value to reverse: ")
-# reversed_value = user_value[::-1]
-# print("Reversed value: ", reversed_value)
-
-
-user_value =input("Enter Any thing to reverse it: ")
-reversed_value = user_value[::-1]
-print("The reversed Value is: ", reversed_value)
