006a174805d87e70f0403503a85bb095c6ab9946
gameongit <146956767+gameongit@users.noreply.github.com>
2023-10-07 00:32:15 +0530

    Delete 1a_age_counter.py

--- 1a_age_counter.py
+++
@@ -1,6 +0,0 @@
-import datetime
-name=input("Hello!Please enter yOur Name: ")
-print("Hello" + name)
-age=int(input("Enter Your Age: "))
-year_now=datetime.datetime.now()
-print("you will turn 100 in " + str(int(100-age)+int(year_now.year)))
